4def978eaa2a95a41f3ab70fbdcb6df465926e75
AMarin1089 <zoroark1089@gmail.com>
2023-03-02 00:39:31 +0200

    First commit

---
+++ UnweWeatherApp/UnweWeatherApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UnweWeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private OpenWeatherService _WeatherWeatherService;
+        public MainPage()
+        {
+            InitializeComponent();
+            _WeatherWeatherService= new OpenWeatherService();
+        }
+
+
+        string GenerateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?q={_cityEntry.Text }";
+            requestUri += "&units=metric"; // or units = metric
+            requestUri += $"&appid={Constants.OpenWeatherMapAPIKey}";
+            return requestUri;
+        }
+
+        public async void OnGetWeatherButtonClicked(object sender, EventArgs args)
+        {
+            if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
+            {
+                WeatherData weatherData = await _WeatherWeatherService.GetWeatherData(
+                    GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
+                BindingContext = weatherData;
+            }
+
+        }
+    }
+}
